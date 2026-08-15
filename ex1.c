f281f3f2e510d595781fa2a409a07c1c1667011f
Mohammad Izaan <mdizaan1192@gmail.com>
2026-08-15 21:11:25 +0530

    ex1

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include<omp.h>
+#include<stdio.h>
+int main(int argc, char* argv[]){
+    #pragma omp parallel
+    {
+        int rank=omp_get_thread_num();
+        int num_processors=omp_get_num_threads();
+        int num
+        printf("Rank/Thread ID: %d, Number of Processors: %d\n",rank, num_processors);
+    }
+return 0;
+}
